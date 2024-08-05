9e4c4d8c415ca95ca006ec106a6fde674182aef6
Jeremy <fortuneolcreamtainl@gmail.com>
2024-08-05 20:22:33 +0800

    增加 : 基本套件

---
+++ YungChingTask/Application/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace YungChingTask.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(config => config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        return services;
+    }
+}
